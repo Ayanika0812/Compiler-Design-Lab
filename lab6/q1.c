1eb0abff028ab76c57de0e5544c096069cdfaa0d
Ayanika Paul <147071109+Ayanika0812@users.noreply.github.com>
2025-02-10 14:29:02 +0530

    Update q1.c

--- lab6/q1.c
+++ lab6/q1.c
@@ -12,13 +12,11 @@ void Tprime();
 void invalid();
 void valid();
 
-// Function to print error and exit
 void invalid() {
     printf("-----------------ERROR!----------------\n");
     exit(0);
 }
 
-// Function to print success and exit
 void valid() {
     printf("----------------SUCCESS!---------------\n");
     exit(0);
@@ -27,24 +25,24 @@ void valid() {
 // S -> a | > | ( T )
 void S() {
     if (str[curr] == 'a') {
-        curr++;
+        curr++; // Consume 'a'
         return;
     }
     else if (str[curr] == '>') {
-        curr++;
+        curr++; // Consume '>'
         return;
     }
     else if (str[curr] == '(') {
-        curr++;
-        T();  // Parsing T inside parentheses
+        curr++;  // Consume '('
+        T();     // Parse T inside parentheses
         if (str[curr] == ')') {
-            curr++;
+            curr++;  // Consume ')'
             return;
         } else {
-            invalid();
+            invalid();  // If no closing ')', it's invalid
         }
     } else {
-        invalid();
+        invalid();  // If no valid starting character, it's invalid
     }
 }
 
@@ -57,8 +55,8 @@ void T() {
 // T' -> , S T' | 𝜖
 void Tprime() {
     if (str[curr] == ',') {
-        curr++;   // Consume the comma
-        S();      // Parse the next S
+        curr++;  // Consume the comma
+        S();     // Parse the next S
         Tprime(); // Recursively parse the next T'
     }
     // If no comma, T' → 𝜖, so we simply return without doing anything
@@ -69,13 +67,13 @@ int main() {
     printf("Enter String: ");
     scanf("%s", str);
 
-    T(); // Start parsing with T
+    S(); // Start parsing with S (the start symbol)
     
     // Check if we've reached the end of the string
     if (str[curr] == '\0') {
-        valid();
+        valid(); // If the parsing is successful
     } else {
-        invalid();
+        invalid(); // If there are extra characters in the input
     }
 
     return 0;
@@ -84,8 +82,9 @@ int main() {
 
 
 /*
-
- ./a.out
+ cc q1.c 
+student@oslab-02:~/220905128/lab6$ ./a.out
 Enter String: (a,a)
 ----------------SUCCESS!---------------
+
 */
