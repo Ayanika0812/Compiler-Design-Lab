bf37dc72f6c28a7202783b9ac3c5eabbd00787cb
Ayanika Paul <147071109+Ayanika0812@users.noreply.github.com>
2025-02-15 21:54:25 +0530

    Update solved.c

--- lab2/solved/solved.c
+++ lab2/solved/solved.c
@@ -1,50 +1,65 @@
 //Program to remove single and multiline comments from a given ‘C’ file.
 #include <stdio.h>
 #include <stdlib.h>
-int main()
-{
-FILE *fa, *fb;
-int ca, cb;
-fa = fopen("solvedinput.c", "r");
-if (fa == NULL){
-printf("Cannot open file \n");
-exit(0); }
-fb = fopen("solvedout.c", "w");
-ca = getc(fa);
-while (ca != EOF)
-{
-if(ca==' ')
-{
-putc(ca,fb);
-while(ca==' ')
-ca = getc(fa);
-}
-if (ca=='/')
-{
-cb = getc(fa);
-if (cb == '/')
-{
-while(ca != '\n')
-ca = getc(fa);
-}
-else if (cb == '*'){
-do
-{
-while(ca != '*')
-ca = getc(fa);
-ca = getc(fa);
-} while (ca != '/');
-}
-else
-{
-putc(ca,fb);
-putc(cb,fb);
-}
-}
-else putc(ca,fb);
-ca = getc(fa);
-}
-fclose(fa);
-fclose(fb);
-return 0;
+
+int main() {
+    FILE *fa, *fb;
+    int ca, cb;
+
+    // Open input C file for reading
+    fa = fopen("solvedinput.c", "r");
+    if (fa == NULL) {
+        printf("Cannot open file \n");
+        exit(0);
+    }
+
+    // Open output file for writing
+    fb = fopen("solvedout.c", "w");
+
+    ca = getc(fa);
+    while (ca != EOF) {
+        // Handle spaces
+        if (ca == ' ') {
+            putc(ca, fb);
+            while (ca == ' ') {
+                ca = getc(fa);
+            }
+        }
+
+        // Handle comments
+        if (ca == '/') {
+            cb = getc(fa);
+            
+            // Single-line comment
+            if (cb == '/') {
+                while (ca != '\n') {
+                    ca = getc(fa);
+                }
+            }
+            // Multi-line comment
+            else if (cb == '*') {
+                do {
+                    while (ca != '*') {
+                        ca = getc(fa);
+                    }
+                    ca = getc(fa);
+                } while (ca != '/');
+            }
+            // Not a comment, write normally
+            else {
+                putc(ca, fb);
+                putc(cb, fb);
+            }
+        } else {
+            putc(ca, fb);
+        }
+        
+        ca = getc(fa);
+    }
+
+    // Close the files
+    fclose(fa);
+    fclose(fb);
+    
+    return 0;
 }
